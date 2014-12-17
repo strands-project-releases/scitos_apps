c29186efc59e32bcdbb8d76d1686fde0cedc5a3a
Chris Burbridge <cburbridge@gmail.com>
2014-12-17 16:47:25 +0000

    Rebase from 'upstream'

--- scitos_docking/src/scitos_docking/CHead.cpp
+++ scitos_docking/src/scitos_docking/CHead.cpp
@@ -1,28 +1,22 @@
-#include "scitos_docking/CLightClient.h"
+#include "scitos_docking/CHead.h"
 
-CLightClient::CLightClient()
+CHead::CHead()
 {
 	status = false;
 	known = false;
 }
 
-CLightClient::~CLightClient()
+CHead::~CHead()
 {
 }
 
-void CLightClient::set(bool what)
+void CHead::set(bool what)
 {
-	// Some robots never saw the light. They feel empty inside...
-	if(ebcport.empty()) {
-		status = what;
-		return;
-	}
-
 	dynamic_reconfigure::ReconfigureRequest srv_req;
 	dynamic_reconfigure::ReconfigureResponse srv_resp;
 	dynamic_reconfigure::BoolParameter param;
 	dynamic_reconfigure::Config conf;
-	param.name = ebcport;
+	param.name = "MCU_24V_Enabled";
 	param.value = status = what;
 	conf.bools.push_back(param);
 	srv_req.config = conf;
@@ -30,7 +24,7 @@ void CLightClient::set(bool what)
 	known = true;
 }
 
-bool CLightClient::get()
+bool CHead::get()
 {
 	if (known==false) ROS_WARN("Status of the lights is uknown at the moment. Turn them on or off first.");
 	return status;
